b164536d7af7a3dbb79c190842b4c727031bd2a0
Tejas07PSK <palashsarkar0007@gmail.com>
2021-09-09 21:26:01 +0530

    First Release

--- mailProcessor.ts
+++ mailProcessor.ts
@@ -2,17 +2,18 @@ import * as nodemailer from 'nodemailer'
 import * as SMTPTransport from 'nodemailer/lib/smtp-transport';
 import * as Mail from 'nodemailer/lib/mailer'
 import { Transporter } from 'nodemailer'
-import { google, Auth } from 'googleapis';
+import { Auth } from 'googleapis';
 import { Handler } from 'aws-lambda';
 
 export const handler: Handler = async function (event: any): Promise <any> {
+    let result: any = null;
     const oauth2Client: Auth.OAuth2Client = new Auth.OAuth2Client(
         (process.env.CLIENT_ID).trim(),
         (process.env.CLIENT_SECRET).trim(),
         "https://developers.google.com/oauthplayground"
     );
     oauth2Client.setCredentials({ 'refresh_token': (process.env.REFRESH_TOKEN).trim() });
-    let smtpTransportOptions: SMTPTransport.Options = {
+    const smtpTransportOptions: SMTPTransport.Options = {
         'service': "gmail",
         'auth': {
             'type': "OAuth2",
@@ -31,15 +32,18 @@ export const handler: Handler = async function (event: any): Promise <any> {
         'html': event.mail.contentHtml
     };
     const smtpTransport: Transporter = nodemailer.createTransport(smtpTransportOptions);
-    let mailerPromiseFunc: any = (resolve: any, reject: any): void => {
+    const mailerPromiseFunc: any = (resolve: any, reject: any): void => {
         smtpTransport.sendMail(mailOptions, (errMail: Error, mailInfo: any): void => {
             (errMail ? reject(errMail) : resolve(mailInfo));
         });
     }
     await new Promise (mailerPromiseFunc).then((data: any): void => {
-        console.log(data);
+        console.log("Mailing promise resolved !!"); console.log(data);
+        result = data;
     }).catch((error: Error): void => {
-        console.log(error);
+        console.log("Mailing promise rejected !!"); console.log(error);
+        result = error;
     }).finally((): void => { console.log("This was a promise !!") });
     smtpTransport.close();
+    return (result);
 };
